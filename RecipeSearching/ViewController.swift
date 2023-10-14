371ddc9d50392db2c51add1c69e96cbc6028e638
minhye9731 <jenney62639731@gmail.com>
2023-10-15 02:19:54 +0900

    Initial Commit

---
+++ RecipeSearching/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeSearching
+//
+//  Created by 강민혜 on 10/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
